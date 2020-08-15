25879974645dba99308674638c069a43ae4718c3
iliyan <iliyan.tsachev@ilnweb.com>
2020-08-15 17:05:48 +0200

    adding particles

--- components/Hero/hero.cmp.jsx
+++ components/Hero/hero.cmp.jsx
@@ -1,21 +1,51 @@
 import "./hero.module.scss";
-import Particles from 'react-particles-js';
+import Particles from "react-particles-js";
 
 const Hero = () => {
   return (
     <div className="hero">
       <Particles
-      params={{
-        particles: {
-          line_linked: {
-            shadow: {
+        className="particles"
+        params={{
+          particles: {
+            number: { value: 6, density: { enable: true, value_area: 800 } },
+            color: { value: "#e6e6e6" },
+            shape: {
+              type: "polygon",
+              stroke: { width: 0, color: "#000" },
+              polygon: { nb_sides: 4 },
+              image: { src: "img/github.svg", width: 100, height: 100 }
+            },
+            opacity: {
+              value: 0.3,
+              random: true,
+              anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+            },
+            size: {
+              value: 160,
+              random: false,
+              anim: { enable: true, speed: 10, size_min: 40, sync: false }
+            },
+            line_linked: {
+              enable: false,
+              distance: 200,
+              color: "#ffffff",
+              opacity: 1,
+              width: 2
+            },
+            move: {
               enable: true,
-              color: "#3CA9D1",
-              blur: 5
+              speed: 8,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: { enable: false, rotateX: 600, rotateY: 1200 }
             }
-          }
-        }
-      }}/>
+          },
+        }}
+      />
     </div>
   );
 };
