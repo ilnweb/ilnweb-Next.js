c206f3f355dca245e13eae24a949887a05f7b814
iliyan <iliyan.tsachev@ilnweb.com>
2020-08-17 15:42:28 +0200

    adding hero png

--- components/Header/header.cmp.jsx
+++ components/Header/header.cmp.jsx
@@ -2,14 +2,17 @@ import "./header.module.scss";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="ilnlogo">
-        <div className="logo_square"/>
-        <img
-          src="https://res.cloudinary.com/ilnphotography/image/upload/v1596117875/ilnweb/ilnLogo_rkqrkr.png"
-          alt="ilnweblogo"
-          className="logo"
-        />
+    <div>
+      <div className="header">
+        <div className="ilnlogo">
+          <div className="logo_square" />
+          <img
+            src="https://res.cloudinary.com/ilnphotography/image/upload/v1596117875/ilnweb/ilnLogo_rkqrkr.png"
+            alt="ilnweblogo"
+            className="logo"
+          />
+          
+        </div>
       </div>
     </div>
   );
--- components/Hero/hero.cmp.jsx
+++ components/Hero/hero.cmp.jsx
@@ -48,9 +48,14 @@ const Hero = () => {
       />
       <div className="hero_content">
         <div className="hero_content-left">
-          <h1>Get the perfect website or web apllication now</h1>
+          <h1>Web developement professional freelance services</h1>
+          <p className="hero_content-left_subtitle">Show your bussines sucsees with a great and modern Progressive Web App, Single page Application or Web Site</p>
+          <button>Get a quote now!</button>
         </div>
-        <div className="hero_content-right"></div>
+        <div className="hero_content-right">
+          <img src="https://res.cloudinary.com/ilnphotography/image/upload/v1597671370/ilnweb%20new/hero_umal1n_1_zktabe.png" alt="" />
+        </div>
+       
       </div>
     </div>
   );
