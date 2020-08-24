ccf6457e90e19129d8eb20d74246f089d5b625d3
iliyan <iliyan.tsachev@ilnweb.com>
2020-08-24 19:34:43 +0200

    adding blur circle and circle lines

--- components/About/about.cmp.jsx
+++ components/About/about.cmp.jsx
@@ -5,7 +5,12 @@ const About = () => {
     <div className="portfolio">
       <h1 className="portfolio-title">Check out some of my work </h1>
       <p className="subtitle">Here you can see some of my favorite project and explore techologies used, video preview and link to the actual project.</p>
-      <div className="portfolio-content"></div>
+      <div className="portfolio-content">
+      <div className="portfolio_backround"></div>
+        <div className="portfolio_circle-1"></div>
+        <div className="portfolio_circle-2"></div>
+        <div className="portfolio_circle-3"></div>
+      </div>
     </div>
   );
 };
