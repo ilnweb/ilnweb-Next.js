fe8b018abe5d2692c6ecb737166b153fe9e7257e
iliyan <iliyan.tsachev@ilnweb.com>
2020-08-24 20:09:12 +0200

    finishing circle animation

--- components/About/about.cmp.jsx
+++ components/About/about.cmp.jsx
@@ -1,15 +1,27 @@
 import "./about.module.scss";
+import { AiTwotoneStar } from "react-icons/ai";
+import { IoMdSquare } from "react-icons/io";
+import { CgShapeTriangle } from "react-icons/cg";
 
 const About = () => {
   return (
     <div className="portfolio">
       <h1 className="portfolio-title">Check out some of my work </h1>
-      <p className="subtitle">Here you can see some of my favorite project and explore techologies used, video preview and link to the actual project.</p>
+      <p className="subtitle">
+        Here you can see some of my favorite project and explore techologies
+        used, video preview and link to the actual project.
+      </p>
       <div className="portfolio-content">
-      <div className="portfolio_backround"></div>
-        <div className="portfolio_circle-1"></div>
-        <div className="portfolio_circle-2"></div>
-        <div className="portfolio_circle-3"></div>
+        <div className="portfolio_backround"></div>
+        <div className="portfolio_circle-1">
+          <AiTwotoneStar className="circle-icon circle-icon-1" />
+          <div className="portfolio_circle-2">
+            <IoMdSquare className="circle-icon circle-icon-2" />
+            <div className="portfolio_circle-3">
+              <CgShapeTriangle className="circle-icon circle-icon-3" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
