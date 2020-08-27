3c2b8ae8fe6b4d0fcd57cdf8c159a818b3cea835
iliyan <iliyan.tsachev@ilnweb.com>
2020-08-27 15:04:26 +0200

    adding react player

--- components/About/about.cmp.jsx
+++ components/About/about.cmp.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "./about.module.scss";
 import { AiTwotoneStar } from "react-icons/ai";
 import { IoMdSquare } from "react-icons/io";
 import { CgShapeTriangle } from "react-icons/cg";
 import { FaDotCircle } from "react-icons/fa";
 import { BsPentagonFill } from "react-icons/bs";
+import { Waypoint } from "react-waypoint";
 
 const About = () => {
+  const [inView, setView] = useState(false);
 
+  const handleWaypointEnter = () => {
+    setView(true);
+  };
+  const handleWaypointLeave = () => {
+    setView(false);
+  };
   return (
     <div className="portfolio">
       <h1 className="portfolio-title">Check out some of my work </h1>
@@ -14,8 +23,8 @@ const About = () => {
         Here you can see some of my favorite project and explore techologies
         used, video preview and link to the actual project.
       </p>
+      
       <div className="portfolio-content">
-        
         <div className="portfolio_backround"></div>
         <div className="portfolio_circle-1">
           <AiTwotoneStar className="circle-icon circle-icon-1" />
@@ -28,7 +37,15 @@ const About = () => {
             </div>
           </div>
         </div>
-        <video className="video-1" width="400" height="260">
+        <Waypoint onEnter={handleWaypointEnter} onLeave={handleWaypointLeave} />
+        <video
+          className={`${inView ? "video-1" : ""}`}
+          width="400"
+          height="260"
+          muted
+          autoPlay={inView}
+          
+        >
           <source
             src="https://res.cloudinary.com/ilnphotography/video/upload/v1598457283/ilnweb/Qizify_vaqgnv.mp4"
             type="video/mp4"
