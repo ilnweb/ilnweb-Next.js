f557f4ca1ae9107e53f1cc387821ac684fa5f1a2
iliyan <iliyan.tsachev@ilnweb.com>
2020-09-04 18:34:48 +0200

    updating inview functionality

--- components/About/about.cmp.jsx
+++ components/About/about.cmp.jsx
@@ -6,13 +6,13 @@ import { CgShapeTriangle } from "react-icons/cg";
 import { FaDotCircle } from "react-icons/fa";
 import { BsPentagonFill } from "react-icons/bs";
 import { Waypoint } from "react-waypoint";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 const About = () => {
   const [inView, setView] = useState(false);
 
   const handleWaypointEnter = () => {
-    setView(false);
+    setView(true);
   };
   const handleWaypointLeave = () => {
     setView(false);
@@ -24,34 +24,34 @@ const About = () => {
         Here you can see some of my favorite project and explore techologies
         used, video preview and link to the actual project.
       </p>
-      
-      <div className="portfolio-content">
-        <div className="portfolio_backround"></div>
-        <div className="portfolio_circle-1">
-          <AiTwotoneStar className="circle-icon circle-icon-1" />
-          <FaDotCircle className="circle-icon circle-icon-4" />
-          <BsPentagonFill className="circle-icon circle-icon-5" />
-          <div className="portfolio_circle-2">
-            <IoMdSquare className="circle-icon circle-icon-2" />
-            <div className="portfolio_circle-3">
-              <CgShapeTriangle className="circle-icon circle-icon-3" />
+      <Waypoint onEnter={handleWaypointEnter} onLeave={handleWaypointLeave} />
+      {inView && (
+        <div className="portfolio-content">
+          <div className="portfolio_backround"/>
+          <div className="portfolio_circle-1">
+            <AiTwotoneStar className="circle-icon circle-icon-1" />
+            <FaDotCircle className="circle-icon circle-icon-4" />
+            <BsPentagonFill className="circle-icon circle-icon-5" />
+            <div className="portfolio_circle-2">
+              <IoMdSquare className="circle-icon circle-icon-2" />
+              <div className="portfolio_circle-3">
+                <CgShapeTriangle className="circle-icon circle-icon-3" />
+              </div>
             </div>
           </div>
+
+          <div className={`video-1 ${inView ? "video-animation" : ""}`}>
+            <ReactPlayer
+              height="400px"
+              muted={true}
+              playing={inView}
+              url="https://res.cloudinary.com/ilnphotography/video/upload/v1598539103/ilnweb/Together_-_Google_Chrome_2020-08-27_16-09-19_1_cjiujt.mp4"
+            >
+              Your browser does not support the video tag.
+            </ReactPlayer>
+          </div>
         </div>
-        <Waypoint onEnter={handleWaypointEnter} onLeave={handleWaypointLeave} />
-        <div className={`video-1 ${inView ? "video-animation" : ""}`}>
-        {inView&&<ReactPlayer
-          height="400px"
-          muted={true}
-          playing={inView}
-          url="https://res.cloudinary.com/ilnphotography/video/upload/v1598539103/ilnweb/Together_-_Google_Chrome_2020-08-27_16-09-19_1_cjiujt.mp4"
-          
-        >
-          Your browser does not support the video tag.
-        </ReactPlayer>}
-        </div>
-        
-      </div>
+      )}
     </div>
   );
 };
