ee6a9bb1c4369c1fbb02e73cfae42b9b267adf78
iliyan <iliyan.tsachev@ilnweb.com>
2020-09-15 15:28:20 +0200

    skiils preparation section

--- components/Skills/skills.cmp.jsx
+++ components/Skills/skills.cmp.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import "./skills.module.scss";
 
 const Skills = () => {
   return (
     <div className="skills">
       <h1>Skills</h1>
+      <p>Here are some of my besht skills and technologies that im user and I love</p>
     </div>
   );
 };
