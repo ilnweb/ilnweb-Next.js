75ae9180dc79fa215408ed406bf614bad1033c33
iliyan <iliyan.tsachev@ilnweb.com>
2020-09-21 15:48:44 +0200

    adding skiils ref

--- components/Skills/skills.cmp.jsx
+++ components/Skills/skills.cmp.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, createRef } from "react";
 import "./skills.module.scss";
 import {
   FaNodeJs,
@@ -22,7 +22,6 @@ import {
 } from "react-icons/si";
 import { AiOutlineAntDesign } from "react-icons/ai";
 import { Waypoint } from "react-waypoint";
-
 
 const Skills = () => {
   const [skills, setSkills] = useState([
@@ -98,6 +97,7 @@ const Skills = () => {
   ]);
 
   const [inView, setView] = useState(false);
+  const skillsLoop = createRef();
 
   const handleWaypointEnter = () => {
     setView(true);
@@ -106,19 +106,6 @@ const Skills = () => {
   const handleWaypointLeave = () => {
     setView(false);
   };
-
-  // useEffect(() => {
-  //   const timeOut = setTimeout(() => {
-  //       return (
-  //         <div key={index} className="skills-icon">
-  //           <TagName className="icon-image" />
-  //           <p>{skill.title}</p>
-  //         </div>
-  //       );
-  //     }, 500);
-  //   }
-  // }, []);
-  
 
   return (
     <div className="skills">
@@ -130,21 +117,29 @@ const Skills = () => {
       <Waypoint onEnter={handleWaypointEnter} onLeave={handleWaypointLeave} />
       <div className="skills-icons">
         {inView &&
-          skills.forEach((skill, index) => {
+          skills.map((skill, index) => {
             let TagName = skill.url;
-            return timeOut(TagName,index,skill)
+            return (
+              <div ref={skillsLoop} key={index} className="skills-icon">
+                <TagName className="icon-image" />
+                <p>{skill.title}</p>
+              </div>
+            );
           })}
-        {
-          //  <div className="skills-icon">
-          //     <svg height="1em" width="1em" viewBox="0 0 128 128" className="icon-image">
-          //       <path
-          //         fill="aliceblue"
-          //         d="M95 2.3l30.5 12.3v98.7L94.8 125.7 45.8 77l-31 24.1L2.5 94.9V33.1l12.3-5.9 31 24.3ZM14.8 45.7V83.2l18.5-19Zm48.1 18.5L94.8 89.3V39Z"
-          //       ></path>
-          //     </svg>
-          //     <p>VS Code</p>
-          //   </div>
-        }
+        <div ref={skillsLoop} className="skills-icon">
+          <svg
+            height="1em"
+            width="1em"
+            viewBox="0 0 128 128"
+            className="icon-image"
+          >
+            <path
+              fill="aliceblue"
+              d="M95 2.3l30.5 12.3v98.7L94.8 125.7 45.8 77l-31 24.1L2.5 94.9V33.1l12.3-5.9 31 24.3ZM14.8 45.7V83.2l18.5-19Zm48.1 18.5L94.8 89.3V39Z"
+            ></path>
+          </svg>
+          <p>VS Code</p>
+        </div>
       </div>
     </div>
   );
