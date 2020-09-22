937b500ef5f9a9f462532949c2515b929453e7c0
iliyan <iliyan.tsachev@ilnweb.com>
2020-09-22 12:45:43 +0200

    improvment of line animation

--- components/About/about.cmp.jsx
+++ components/About/about.cmp.jsx
@@ -16,18 +16,25 @@ const About = () => {
   return (
     <div className="about">
       <h1>About</h1>
-      <Waypoint onEnter={handleWaypointEnter} onLeave={handleWaypointLeave}/>
+      <Waypoint onEnter={handleWaypointEnter} onLeave={handleWaypointLeave} />
       <div className="about-content">
         <div className="about-content-left">
           <h1>Hi, my name is ILIYAN TSACHEV</h1>
           <p className="subtitle">
-            I'm a JavaScript developer based in Warsaw, Poland. I spacialize in Front-End technologies like JavaScript, React, CSS, HTML, SASS, Single Page Aplications and Progresive Web Aplications. I have a strong knowedge in Back-End technologies and NoSQL databases.
+            I'm a JavaScript developer based in Warsaw, Poland. I spacialize in
+            Front-End technologies like JavaScript, React, CSS, HTML, SASS,
+            Single Page Aplications and Progresive Web Aplications. I have a
+            strong knowedge in Back-End technologies and NoSQL databases.
           </p>
-          
         </div>
         <div className="about-content-right">
-          <div className="about-line" />
-          <img className={`img ${inView ? "image-show":''}`} style={{width:'90%'}} src="https://res.cloudinary.com/ilnphotography/image/upload/v1600702880/ilnweb/shadow_zsw6h2_royosx.png" alt=""/>
+          <div className={`"about-line" ${inView ? "about-line-animation" : ""}`}/>
+          <img
+            className={`img ${inView ? "image-show" : ""}`}
+            style={{ width: "90%" }}
+            src="https://res.cloudinary.com/ilnphotography/image/upload/v1600702880/ilnweb/shadow_zsw6h2_royosx.png"
+            alt=""
+          />
         </div>
       </div>
     </div>
