76c99bb1e044687da22c825871cb335ed2937352
iliyan <iliyan.tsachev@ilnweb.com>
2020-10-12 19:30:31 +0200

    separating logo in a component

--- components/Layout/layout.cmp.jsx
+++ components/Layout/layout.cmp.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Head from "next/head";
 import Header from "../Header/header.cmp";
-import './layout.module.scss'
+import "./layout.module.scss";
 
 export default function Layout({ children, title = "ilnweb" }) {
   return (
@@ -10,12 +10,22 @@ export default function Layout({ children, title = "ilnweb" }) {
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;700&display=swap" rel="stylesheet"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Exo+2:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Header />
       {children}
       <footer className="footer">
-        
+        <div className="ilnlogo">
+          <div className="logo_square" />
+          <img
+            src="https://res.cloudinary.com/ilnphotography/image/upload/v1596117875/ilnweb/ilnLogo_rkqrkr.png"
+            alt="ilnweblogo"
+            className="logo"
+          />
+        </div>
       </footer>
     </div>
   );
