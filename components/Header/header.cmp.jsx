5cd84420268f3fd77a9a05a45e034a6b8ef09452
iliyan <iliyan.tsachev@ilnweb.com>
2020-10-12 19:33:48 +0200

    finishing logo ajusmento for reusability

--- components/Header/header.cmp.jsx
+++ components/Header/header.cmp.jsx
@@ -1,16 +1,10 @@
 import "./header.module.scss";
+import Logo from "../Logo/logo.cmp";
 
 const Header = () => {
   return (
       <div className="header">
-        <div className="ilnlogo">
-          <div className="logo_square" />
-          <img
-            src="https://res.cloudinary.com/ilnphotography/image/upload/v1596117875/ilnweb/ilnLogo_rkqrkr.png"
-            alt="ilnweblogo"
-            className="logo"
-          /> 
-        </div>
+        <Logo/>
         <button className="button-secondary">Hire me now!</button>
       </div>
   );
---
+++ components/Logo/logo.cmp.jsx
@@ -0,0 +1,16 @@
+import "./logo.module.scss"
+
+const Logo = () => {
+  return (
+    <div className="ilnlogo">
+          <div className="logo_square" />
+          <img
+            src="https://res.cloudinary.com/ilnphotography/image/upload/v1596117875/ilnweb/ilnLogo_rkqrkr.png"
+            alt="ilnweblogo"
+            className="logo"
+          /> 
+        </div>
+  )
+}
+
+export default Logo;
