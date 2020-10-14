7aaa22ff5fd4353e39b1e5af5df57307ff446d3b
iliyan <iliyan.tsachev@ilnweb.com>
2020-10-14 16:29:25 +0200

    creating logo and nav links

--- components/Logo/logo.cmp.jsx
+++ components/Logo/logo.cmp.jsx
@@ -1,16 +1,19 @@
-import "./logo.module.scss"
+import "./logo.module.scss";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <div className="ilnlogo">
-          <div className="logo_square" />
-          <img
-            src="https://res.cloudinary.com/ilnphotography/image/upload/v1602679745/ilnweb/ilnLogo-grey-black_ee79eg.png"
-            alt="ilnweblogo"
-            className="logo"
-          /> 
-        </div>
-  )
-}
+    <Link href="/">
+      <div className="ilnlogo">
+        <div className="logo_square" />
+        <img
+          src="https://res.cloudinary.com/ilnphotography/image/upload/v1602679745/ilnweb/ilnLogo-grey-black_ee79eg.png"
+          alt="ilnweblogo"
+          className="logo"
+        />
+      </div>
+    </Link>
+  );
+};
 
 export default Logo;
--- components/nav-desk/nav-desk.cmp.jsx
+++ components/nav-desk/nav-desk.cmp.jsx
@@ -1,11 +1,12 @@
 import "./nav-desk.module.scss";
+import Link from 'next/link';
 
 const NavDesk = () => {
   return (
     <div className="nav-desk">
       <ul className="nav-desk-list">
         <li>Home</li>
-        <li>Portfolio</li>
+        <Link href="/portfolio"><li>Portfolio</li></Link>
         <li>About</li>
         <li>Skills</li>
       </ul>
