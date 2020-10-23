00e1442e738ced712a65cf5fdfcdfce299fd4f26
iliyan <iliyan.tsachev@ilnweb.com>
2020-10-23 14:12:27 +0200

    testing third graphic

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -21,7 +21,7 @@ const Portfolio = () => {
           <div className="porfolio-page_item-left">
             <div>
               <img
-                style={{ width: "100%" }}
+                style={{ width: "100%"}}
                 src="https://res.cloudinary.com/ilnphotography/image/upload/v1602862319/ilnweb/Qizify-back2_k2pjfp.png"
               />
               <img
@@ -93,6 +93,43 @@ const Portfolio = () => {
             </div>
           </div>
         </div>
+        <div className="porfolio-page_item">
+        <div className="porfolio-page_item-left">
+          <div>
+            <img
+              style={{ width: "100%"}}
+              src="https://res.cloudinary.com/ilnphotography/image/upload/v1603450112/ilnweb/Artboard_1_usldor.png"
+            />
+            <img
+              className="desctop"
+              src="https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png"
+            />
+            <img
+              className="mobile"
+              src="https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png"
+            />
+          </div>
+        </div>
+        <div className="porfolio-page_item-right">
+          <h3 style={{ color: "grey", marginTop: "0rem" }}>Qizify</h3>
+          <h2 style={{ fontSize: "2rem", marginTop: "0rem" }}>
+            A Quiz game Application that aloowes you to play in 3 languages.
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of
+            Lorem Ipsum.
+          </p>
+          <div style={{ color: "#644596" }}>View Project</div>
+        </div>
+      </div>
       </div>
     </Layout>
   );
