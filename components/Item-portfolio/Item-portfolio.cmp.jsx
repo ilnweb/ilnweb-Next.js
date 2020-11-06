153e404a40d32a877f2912e6c750839f92ea0958
iliyan <iliyan.tsachev@ilnweb.com>
2020-11-06 18:42:14 +0100

    finishing portfolio layout

--- components/Item-portfolio/Item-portfolio.cmp.jsx
+++ components/Item-portfolio/Item-portfolio.cmp.jsx
@@ -12,25 +12,23 @@ const ItemPortfolio = ({item}) => (
         }
         <img
           className="desctop"
-          style={{ border: "3px solid white" }}
-          src="https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png"
+          src={item.desctop}
         />
         <img
           className="mobile"
-          style={{ border: "3px solid white" }}
-          src="https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png"
+          src={item.mobile}
         />
       </div>
     </div>
     <div className="portfolio-page_item-right">
-      <h3 style={{ color: "grey", marginTop: "0rem" }}>{item.title}</h3>
-      <h2 style={{ fontSize: "2rem", marginTop: "0rem" }}>
+      <h2 style={{ color: "#00dcc0", marginBootom: "20px" }}>{item.title}</h2>
+      <h1>
        {item.subTitle}
-      </h2>
+      </h1>
       <p>
         {item.description}
         </p>
-      <div style={{ color: "#644596" }}>View Project</div>
+      <button className="button-secondary">View Project</button>
     </div>
   </div>
 );
--- public/data.js
+++ public/data.js
@@ -3,11 +3,10 @@ export const portfolioData = [
     title: "Qizify",
     subTitle:
       " A Quiz game Application that aloowes you to play in 3 languages.",
-    background:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1604651204/ilnweb/triangle1_x9yzum.png",
-    mobile:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    background:"https://res.cloudinary.com/ilnphotography/image/upload/v1604652911/ilnweb/circle1_evxgd9.png",
     desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    mobile:
       "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
     description:
       "Qizify Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
@@ -18,11 +17,13 @@ export const portfolioData = [
     subTitle:
       "Social media application inspired by my favorite sited Medium and DEV.",
     background:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1604652911/ilnweb/circle1_evxgd9.png",
-    mobile:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604651204/ilnweb/triangle1_x9yzum.png",
     desctop:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604675232/ilnweb/Screenshot_231_df5gsn.png",
+
+    mobile:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604675650/ilnweb/Screenshot_233_vgkwya.png",
+
     description:
       "Dev SOcial Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
     color: "red",
@@ -33,9 +34,9 @@ export const portfolioData = [
       "An application where you and your friends can keep track of mutual spendings.",
     background:
       "https://res.cloudinary.com/ilnphotography/image/upload/v1604654596/ilnweb/squaire1_nszow7.png",
-    mobile:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
     desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    mobile:
       "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
     description:
       "Togheder Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
@@ -43,12 +44,12 @@ export const portfolioData = [
   },
   {
     title: "Crown clothing",
-    subTitle: "Ready to deploy ecommerse template perfect for smaal bussiness.",
+    subTitle: "Ecommerse template perfect for small bussiness.",
     background:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1604654596/ilnweb/squaire1_nszow7.png",
-    mobile:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604651204/ilnweb/triangle1_x9yzum.png",
     desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    mobile:
       "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
     description:
       "Crown Clothing is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
@@ -58,10 +59,10 @@ export const portfolioData = [
     title: "iliyan tsachev photography",
     subTitle: "My personal photography page.",
     background:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1604654596/ilnweb/squaire1_nszow7.png",
-    mobile:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604652911/ilnweb/circle1_evxgd9.png",
     desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    mobile:
       "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
     description:
       "Crown Clothing is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
