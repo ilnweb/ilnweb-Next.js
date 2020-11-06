d230146f45bb7cbcd0837ecb12fc56081d2a86d6
iliyan <iliyan.tsachev@ilnweb.com>
2020-11-06 20:17:56 +0100

    finishing portfolio animation items

--- components/Item-portfolio/Item-portfolio.cmp.jsx
+++ components/Item-portfolio/Item-portfolio.cmp.jsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import "./Item-portfolio.module.scss";
+import { Waypoint } from "react-waypoint";
 
-const ItemPortfolio = ({item}) => (
-  <div className="portfolio-page_item">
+const ItemPortfolio = ({ item }) => {
+  const [inView, setView] = useState(false);
+  const handleWaypointEnter = () => {
+    setView(true);
+  };
+  const handleWaypointLeave = () => {
+    setView(false);
+  };
+  return (
+    <div className="portfolio-page_item">
+    <Waypoint onEnter={handleWaypointEnter} onLeave={handleWaypointLeave} />
     <div className="portfolio-page_item-left">
       <div>
         {
           <img
-        className="portfolio-page_item-left_backround"
-          src={item.background}
+            className="portfolio-page_item-left_backround"
+            src={item.background}
           />
         }
         <img
-          className="desctop"
+          className={`${item.mobileOnly ? "mobileOnly":"desctop"} ${inView ? item.mobile?"mobileOnlyInview": "opacity desctopInview ":""}`}
           src={item.desctop}
         />
         <img
-          className="mobile"
+          className={`mobile ${inView ? "opacity mobileInview opacity":""}`}
           src={item.mobile}
         />
       </div>
@@ -23,14 +34,15 @@ const ItemPortfolio = ({item}) => (
     <div className="portfolio-page_item-right">
       <h2 style={{ color: "#00dcc0", marginBootom: "20px" }}>{item.title}</h2>
       <h1>
-       {item.subTitle}
+        {item.subTitle}
       </h1>
       <p>
         {item.description}
-        </p>
+      </p>
       <button className="button-secondary">View Project</button>
     </div>
-  </div>
-);
+    </div>
+  )
+};
 
 export default ItemPortfolio;
--- public/data.js
+++ public/data.js
@@ -35,12 +35,13 @@ export const portfolioData = [
     background:
       "https://res.cloudinary.com/ilnphotography/image/upload/v1604654596/ilnweb/squaire1_nszow7.png",
     desctop:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604689549/ilnweb/Screenshot_236_fldtxr.png",
     mobile:
-      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604689549/ilnweb/Screenshot_235_iwf3pi.png",
     description:
       "Togheder Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
     color: "blue",
+    mobileOnly:true
   },
   {
     title: "Crown clothing",
