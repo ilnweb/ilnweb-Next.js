2d4ae08b3af0ad86a226b450f01cb5fe5919477f
iliyan <iliyan.tsachev@ilnweb.com>
2020-11-06 12:22:46 +0100

    finishing up data for portfolio items

--- public/data.js
+++ public/data.js
@@ -1,11 +1,70 @@
 export const portfolioData = [
   {
     title: "Qizify",
-    subTitle:" A Quiz game Application that aloowes you to play in 3 languages.",
-    background:"https://res.cloudinary.com/ilnphotography/image/upload/v1604651204/ilnweb/triangle1_x9yzum.png",
-    mobile:"https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
-    desctop: "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
-    description: "Qizify Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+    subTitle:
+      " A Quiz game Application that aloowes you to play in 3 languages.",
+    background:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604651204/ilnweb/triangle1_x9yzum.png",
+    mobile:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
+    description:
+      "Qizify Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
     color: "purple",
-  }
-]
+  },
+  {
+    title: "Dev",
+    subTitle:
+      "Social media application inspired by my favorite sited Medium and DEV.",
+    background:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604652911/ilnweb/circle1_evxgd9.png",
+    mobile:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
+    description:
+      "Dev SOcial Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+    color: "red",
+  },
+  {
+    title: "Togheder",
+    subTitle:
+      "An application where you and your friends can keep track of mutual spendings.",
+    background:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604654596/ilnweb/squaire1_nszow7.png",
+    mobile:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
+    description:
+      "Togheder Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+    color: "blue",
+  },
+  {
+    title: "Crown clothing",
+    subTitle: "Ready to deploy ecommerse template perfect for smaal bussiness.",
+    background:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604654596/ilnweb/squaire1_nszow7.png",
+    mobile:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
+    description:
+      "Crown Clothing is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+    color: "green",
+  },
+  {
+    title: "iliyan tsachev photography",
+    subTitle: "My personal photography page.",
+    background:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1604654596/ilnweb/squaire1_nszow7.png",
+    mobile:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_221_rk9fxf.png",
+    desctop:
+      "https://res.cloudinary.com/ilnphotography/image/upload/v1602862830/ilnweb/Screenshot_2020-10-16_173816_weptbq.png",
+    description:
+      "Crown Clothing is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap",
+    color: "red",
+  },
+];
