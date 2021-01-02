77ad46aa4a16cf90e9da3a73eb94edf551a03412
iliyan <iliyan.tsachev@ilnweb.com>
2021-01-02 14:44:06 +0100

    adding the sticky nav functionality

--- components/Header/header.cmp.jsx
+++ components/Header/header.cmp.jsx
@@ -1,4 +1,5 @@
 import "./header.module.scss";
+import { useEffect, useState } from "react";
 import Logo from "../Logo/logo.cmp";
 import NavDesk from "../nav-desk/nav-desk.cmp";
 import { Link } from "react-scroll";
@@ -6,11 +7,29 @@ import { Link } from "react-scroll";
 const Header = () => {
   const scroll = Link;
 
+  const [nav, setNav] = useState({ nav: false });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 600) {
+        setNav({ nav: true });
+      } else if (window.pageYOffset < 600) {
+        setNav({ nav: false });
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [nav]);
+
   return (
-    <div id="header" className="header">
+    <div id="header" className={`header ${nav.nav ? "sticky" : ""}`}>
       <Logo
         url={
-          "https://res.cloudinary.com/ilnphotography/image/upload/v1602679432/ilnweb/ilnLogo_white-rkqrkr_z7dwn7.png"
+          nav.nav ?
+              "https://res.cloudinary.com/ilnphotography/image/upload/v1602679745/ilnweb/ilnLogo-grey-black_ee79eg.png" : "https://res.cloudinary.com/ilnphotography/image/upload/v1602679432/ilnweb/ilnLogo_white-rkqrkr_z7dwn7.png"
         }
       />
       <NavDesk Scroll={scroll} />
--- components/nav-desk/nav-desk.cmp.jsx
+++ components/nav-desk/nav-desk.cmp.jsx
@@ -1,12 +1,11 @@
 import "./nav-desk.module.scss";
-import Link from 'next/link'
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { RiArrowGoBackLine } from "react-icons/ri";
 
-
-const NavDesk = ({Scroll}) => {
+const NavDesk = ({ Scroll }) => {
   const router = useRouter();
-  console.log(router.pathname);
+ 
 
   return (
     <div className="nav-desk">
@@ -22,13 +21,31 @@ const NavDesk = ({Scroll}) => {
             <Scroll activeClass="active" to="home">
               <li>Home</li>
             </Scroll>
-            <Scroll to="portfolio" spy={true} smooth={true}>
+            <Scroll
+              to="portfolio"
+              spy={true}
+              smooth={true}
+              duration={500}
+              hashSpy={true}
+            >
               <li>Portfolio</li>
             </Scroll>
-            <Scroll to="about" spy={true} smooth={true}>
+            <Scroll
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              hashSpy={true}
+            >
               <li>About</li>
             </Scroll>
-            <Scroll to="skills" spy={true} smooth={true}>
+            <Scroll
+              to="skills"
+              spy={true}
+              smooth={true}
+              duration={500}
+              hashSpy={true}
+            >
               <li>Skills</li>
             </Scroll>
           </>
